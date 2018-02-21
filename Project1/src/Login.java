894aaedd43a2ff72639295e7966962480b9d4e84
Chanun Sumphanphaisal <csumphan@uci.edu>
2018-02-20 22:29:32 -0800

    add built react app to server

--- Project1/src/Login.java
+++ Project1/src/Login.java
@@ -34,6 +34,7 @@ public class Login extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -54,78 +55,92 @@ public class Login extends HttpServlet {
         
         String email = request.getParameter("email");
         String password = request.getParameter("password");
+        String gRecaptchaResponse = request.getParameter("recaptcha");
+    		System.out.println("gRecaptchaResponse=");
+    		System.out.println(gRecaptchaResponse);
         String jsonLine = "{\"movieId\":\"tt0100275\",\"count\":1}";
         JsonParser parser = new JsonParser();
         JsonObject o = parser.parse(jsonLine).getAsJsonObject();
         
         System.out.println(o);
-        System.out.println("hihihi");
+        System.out.println("fuck");
         System.out.println(o.get("movieId"));
         response.setContentType("application/json"); // Response mime type
+        boolean valid = VerifyUtils.verify(gRecaptchaResponse);
+        System.out.println("valid");
+        System.out.println(valid);
         
         PrintWriter out = response.getWriter();
-        
-        try {
-            //Class.forName("org.gjt.mm.mysql.Driver");
-        		Class.forName("com.mysql.jdbc.Driver").newInstance();
+        if (valid) {
+        		try {
+                //Class.forName("org.gjt.mm.mysql.Driver");
+            		Class.forName("com.mysql.jdbc.Driver").newInstance();
 
-            Connection dbcon = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
-            
-            // Declare our statement
-            Statement statement = dbcon.createStatement();
-            
-            // Check if email exists
-            String existsQuery = "SELECT * FROM customers WHERE email = '" + email + "' AND password = '" + password + "'";
-            
-            ResultSet rs = statement.executeQuery(existsQuery);
-            
-            if (rs.next()) {
-            		JsonObject userInfo = new JsonObject();
-            		
-            		String m_id = rs.getString("id");
-            		String m_firstName = rs.getString("firstName");
-            		String m_lastName = rs.getString("lastName");
-            		String m_ccId = rs.getString("ccId");
-            		String m_address = rs.getString("address");
-            		String m_email = rs.getString("email");
-            		String m_password = rs.getString("password");
-            		
-            		userInfo.addProperty("id", m_id);
-            		userInfo.addProperty("firstName", m_firstName);
-            		userInfo.addProperty("lastName", m_lastName);
-            		userInfo.addProperty("ccId", m_ccId);
-            		userInfo.addProperty("address", m_address);
-            		userInfo.addProperty("email", m_email);
-            		userInfo.addProperty("password", m_password);
-            		
-            		request.getSession().setAttribute("id", m_id);
-            		request.getSession().setAttribute("firstName", m_firstName);
-            		request.getSession().setAttribute("lastName", m_lastName);
-            		
-            		out.write(userInfo.toString());
+                Connection dbcon = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
+                
+                // Declare our statement
+                Statement statement = dbcon.createStatement();
+                
+                // Check if email exists
+                String existsQuery = "SELECT * FROM customers WHERE email = '" + email + "' AND password = '" + password + "'";
+                
+                ResultSet rs = statement.executeQuery(existsQuery);
+                
+                if (rs.next()) {
+                		JsonObject userInfo = new JsonObject();
+                		
+                		String m_id = rs.getString("id");
+                		String m_firstName = rs.getString("firstName");
+                		String m_lastName = rs.getString("lastName");
+                		String m_ccId = rs.getString("ccId");
+                		String m_address = rs.getString("address");
+                		String m_email = rs.getString("email");
+                		String m_password = rs.getString("password");
+                		
+                		userInfo.addProperty("id", m_id);
+                		userInfo.addProperty("firstName", m_firstName);
+                		userInfo.addProperty("lastName", m_lastName);
+                		userInfo.addProperty("ccId", m_ccId);
+                		userInfo.addProperty("address", m_address);
+                		userInfo.addProperty("email", m_email);
+                		userInfo.addProperty("password", m_password);
+                		
+                		request.getSession().setAttribute("id", m_id);
+                		request.getSession().setAttribute("firstName", m_firstName);
+                		request.getSession().setAttribute("lastName", m_lastName);
+                		
+                		out.write(userInfo.toString());
+                }
+                
+                else {
+                		JsonObject errorInfo = new JsonObject();
+                		errorInfo.addProperty("error", "Incorrect email/password");
+                		
+                		out.write(errorInfo.toString());
+                }
+                
+                rs.close();
+                statement.close();
+                dbcon.close();
+            } catch (SQLException ex) {
+                while (ex != null) {
+                    System.out.println("SQL Exception:  " + ex.getMessage());
+                    ex = ex.getNextException();
+                } // end while
+            } // end catch SQLException
+            catch (java.lang.Exception ex) {
+                out.println("<HTML>" + "<HEAD><TITLE>" + "MovieDB: Error" + "</TITLE></HEAD>\n<BODY>"
+                        + "<P>SQL error in doGet: " + ex.getMessage() + "</P></BODY></HTML>");
+                return;
             }
-            
-            else {
-            		JsonObject errorInfo = new JsonObject();
-            		errorInfo.addProperty("error", "Incorrect email/password");
-            		
-            		out.write(errorInfo.toString());
-            }
-            
-            rs.close();
-            statement.close();
-            dbcon.close();
-        } catch (SQLException ex) {
-            while (ex != null) {
-                System.out.println("SQL Exception:  " + ex.getMessage());
-                ex = ex.getNextException();
-            } // end while
-        } // end catch SQLException
-        catch (java.lang.Exception ex) {
-            out.println("<HTML>" + "<HEAD><TITLE>" + "MovieDB: Error" + "</TITLE></HEAD>\n<BODY>"
-                    + "<P>SQL error in doGet: " + ex.getMessage() + "</P></BODY></HTML>");
-            return;
         }
+        else {
+        	JsonObject errorInfo = new JsonObject();
+    		errorInfo.addProperty("error", "Invalid Recaptcha: Are you human?");
+    		
+    		out.write(errorInfo.toString());
+        }
+        
         out.close();
 		doGet(request, response);
 	}
