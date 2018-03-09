7dcba736879811db5c3da2227cc685a5c3408ecc
Chanun Sumphanphaisal <csumphan@uci.edu>
2018-03-08 22:27:00 -0800

    add full text search

--- fablixFrontEnd/src/containers/MovieListPage/selectors.js
+++ fablixFrontEnd/src/containers/MovieListPage/selectors.js
@@ -2,4 +2,5 @@ export const selectMoviesContainer = (state) => state.containers.moviesReducer;
 
 // Need to use .get, beucase reducer defaulState was created by using ImmutableJS
 export const selectMoviesData = (state) => selectMoviesContainer(state).moviesData;
+export const selectStarsData = (state) => selectMoviesContainer(state).starsData;
 export const selectSearchMoviesError = (state) => selectMoviesContainer(state).searchMoviesError;
