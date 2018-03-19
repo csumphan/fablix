cf4a4ddda73dfb65aa98004754ec5daadfb5d8a7
Elton Xue <eltonxue@gmail.com>
2018-03-18 19:23:49 -0700

    Project 5 task 3

---
+++ apache-tomcat-8.5.24/wtpwebapps/Project1/query_measurements.py
@@ -0,0 +1,28 @@
+file = open("query_measurements.txt", "r")
+
+TS_list = []
+TJ_list = []
+
+queries_run = 0
+
+while True:
+    line1 = file.readline()
+    line2 = file.readline()
+    if not line2:
+        break
+
+    line1Split = line1.split(';')
+    line2Split = line2.split(';')
+
+
+    TS = int(line1Split[0]) + int(line2Split[0])
+    TJ = int(line1Split[1]) + int(line2Split[1])
+
+    TS_list.append(TS)
+    TJ_list.append(TJ)
+
+    queries_run += 1
+
+print("Total Queries: " + str(queries_run))
+print("Average TS: " + str(sum(TS_list) / len(TS_list)))
+print("Average TJ: " + str(sum(TJ_list) / len(TJ_list)))
