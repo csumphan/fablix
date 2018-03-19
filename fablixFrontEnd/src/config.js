a9105fbe24bb94acc3fda8123cda4d81ababc588
Chanun Sumphanphaisal <csumphan@uci.edu>
2018-03-18 19:58:00 -0700

    updated api url

--- fablixFrontEnd/src/config.js
+++ fablixFrontEnd/src/config.js
@@ -1,7 +1,7 @@
 const api =
   process.env.NODE_ENV === 'development'
     ? 'http://127.0.0.1:8080/Project1'
-    : `http://${window.location.hostname}/Project1`;
+    : `http://${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/Project1`;
 
 console.log('apiiii', api);
 
